c0c084c25f680f53748326d22b1f4d311053edb4
manoot <travis.mathis@millicorp.com>
2010-11-30 12:09:22 -0500

    adding copyright info

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Millicorp"
+maintainer_email "travis.mathis@millicorp.com"
 license          "All rights reserved"
-description      "Installs/Configures scaffold"
+description      "Scaffold Recipe for Xen Fog Extension"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
